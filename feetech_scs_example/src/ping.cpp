6505600fb4a02fcbe1f1920365438fbdbc8e1263
Ar-Ray-code <ray255ar@gmail.com>
2023-07-29 16:50:49 +0900

    fix src

--- feetech_scs_example/src/ping.cpp
+++ feetech_scs_example/src/ping.cpp
@@ -19,19 +19,25 @@ int main(int argc, char ** argv)
   int TARGET_ID = 1;
 
   rclcpp::init(argc, argv);
-  auto port_handler = std::make_shared<h6x_serial_interface::PortHandler>("/dev/ttyUSB0", 1000000);
+  auto port_handler = std::make_shared<h6x_serial_interface::PortHandler>("/dev/ttyUSB0");
   auto packet_handler = std::make_shared<feetech_scs_interface::PacketHandler>(port_handler);
 
-  if (!port_handler->openPort()) {
+  port_handler->configure(1000000);
+  if (!port_handler->open()) {
     return EXIT_FAILURE;
   }
+  std::cout << "open success" << std::endl;
 
   using namespace std::chrono_literals;  // NOLINT
   for (int i = 0; i < 3; ++i) {
-    packet_handler->ping(TARGET_ID);
+    if (packet_handler->ping(TARGET_ID)) {
+      std::cout << "ping success" << std::endl;
+    } else {
+      std::cout << "ping fail" << std::endl;
+    }
     rclcpp::sleep_for(1s);
   }
 
-  port_handler->closePort();
+  port_handler->close();
   return EXIT_SUCCESS;
 }
